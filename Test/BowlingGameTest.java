a76a996471c82405c942238dd0549f5e39d34bd2
Arpit Shah <arpit@MavenHives-MacBook-Pro.local>
2018-08-08 03:28:32 +0530

    Add spare roll logic

--- Test/BowlingGameTest.java
+++ Test/BowlingGameTest.java
@@ -6,14 +6,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BowlingGameTest {
 
+  private BowlingGame bowlingGame;
+
   @BeforeEach
   void setUp() {
+    bowlingGame = new BowlingGame();
   }
 
   @AfterEach
   void tearDown() {
+    bowlingGame = null;
   }
 
   @Test
+  public void expectsZeroWhenAllGutterRolls() {
+    rollPins(0, 20);
+    assertEquals(0, bowlingGame.score());
+  }
 
+  @Test
+  public void expectsTwentyWhenAllOneRolls() {
+    rollPins(1, 20);
+    assertEquals(20, bowlingGame.score());
+  }
+
+  @Test
+  public void canRollSpare() {
+    bowlingGame.roll(5);
+    bowlingGame.roll(5);
+    bowlingGame.roll(3);
+    rollPins(0, 17);
+    assertEquals(16, bowlingGame.score());
+  }
+
+  private void rollPins(int rolls, int pins) {
+    for(int i = 0; i < rolls; i++) {
+      bowlingGame.roll(pins);
+    }
+  }
 }
--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,2 +1,29 @@
 public class BowlingGame {
+
+  private int currentRoll = 0;
+  private int[] rolls = new int[21];
+
+  public void roll(int pins) {
+    rolls[currentRoll++] = pins;
+  }
+
+  public int score() {
+    int score = 0;
+    int rollIndex = 0;
+
+    for(int frameIndex = 0; frameIndex < 10; frameIndex++) {
+      if(isSpare(rollIndex)) {
+        score += rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+      } else {
+        score += rolls[rollIndex] + rolls[rollIndex + 1];
+      }
+      rollIndex += 2;
+    }
+
+    return score;
+  }
+
+  private boolean isSpare(int rollIndex) {
+    return rolls[rollIndex] + rolls[rollIndex+1] == 10;
+  }
 }
